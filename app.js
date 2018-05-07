d243afe62ce1cdc4823ee6d3cb68c77445c846e8
Joseph Kim <gyuhankim@gmail.com>
2018-05-07 14:58:10 -0400

    app.js is jediNames

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-let myTeam = " Joshep  Angelo suman";
-console.log(myTeam);
+'use strict';
+
+function jediName(firstName, lastName) {
+    let first = firstName.split('');
+    let last = lastName.split('');
+    let jediLetters = last.slice(0,3).join('') + first.slice(0,2).join('');
+    //console.log(first.slice(0,2));
+    //console.log(last.slice(0,3));
+    console.log(jediLetters);
+}
+
+jediName('Joseph', 'Kim');
