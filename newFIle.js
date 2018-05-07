f58f7d9a5d6a91a38741c4868bdc7761a4972f81
Suman Bandita <sumanbandita@gmail.com>
2018-05-07 14:12:39 -0400

    try and catch

--- newFIle.js
+++ newFIle.js
@@ -1,9 +1,19 @@
 'use strict';
 
 function whoAmI(name,age) {
-  let yob = yearOfBirth(age);
-  console.log(`Hi, my name is ${name} and I\'m ${age} years old`);
-  console.log(`I was born in ${yob}`);
+  try{
+    if(age < 0){
+      throw err;
+    }      
+    else{
+      let yob = yearOfBirth(age);
+      console.log(`Hi, my name is ${name} and I\'m ${age} years old`);
+      console.log(`I was born in ${yob}`);
+    }
+  }catch(err)
+  {
+    throw new Error('Age cannot be negative');
+  }  
 }
 
 function yearOfBirth(age) {
