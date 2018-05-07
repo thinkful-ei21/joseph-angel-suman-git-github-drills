5f141bd7e3fd35037cc85ba4c8cacd8b528029d6
chagoy <angelochagoy@gmail.com>
2018-05-07 11:31:28 -0700

    validation

--- newFIle.js
+++ newFIle.js
@@ -1,27 +1,29 @@
 'use strict';
 
 function whoAmI(name,age) {
-  try{
-    if (typeof name !== 'string' || typeof age !== 'number'){
-      console.error('Arguments not valid.');
-      throw err;
-    }
-    if(age < 0){
-      throw err;
-    }      
-    else{
-      let yob = yearOfBirth(age);
-      console.log(`Hi, my name is ${name} and I\'m ${age} years old`);
-      console.log(`I was born in ${yob}`);
-    }
-  }catch(err)
-  {
-    throw new Error('Age cannot be negative');
-  }  
+    validate(name, age);
+  
+    let yob = yearOfBirth(age);
+      
+    console.log(`Hi, my name is ${name} and I\'m ${age} years old`);
+    console.log(`I was born in ${yob}`); 
 }
 
 function yearOfBirth(age) {
   return 2018 - age;
 }
 
-whoAmI(19, 'suman');
+function validate(name, age) {
+    try {
+        if (typeof name !== 'string' || typeof age !== 'number') {
+            throw err;
+        }
+        if (age < 0) {
+            throw err;
+        }
+    } catch (err) {
+        throw new Error('Arguments not valid');
+    }
+}
+
+whoAmI('suman', 19);
