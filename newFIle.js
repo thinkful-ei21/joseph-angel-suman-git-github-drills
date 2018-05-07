2812d8c75c3e7bd140ae03bfababc22dae57ee14
Joseph Kim <gyuhankim@gmail.com>
2018-05-07 14:03:44 -0400

    did stuff to the js

--- newFIle.js
+++ newFIle.js
@@ -1,11 +1,9 @@
 'use strict';
 
 function whoAmI(name,age) {
-    console.log(`Hi, my name is ${name} and I\'m ${age} years old`);
-
-    
-    
-    
-};
+  let yearOfBirth = 2018 - age;
+  console.log(`Hi, my name is ${name} and I\'m ${age} years old`);
+  console.log(`I was born in ${yearOfBirth}`);
+}
 
 whoAmI('suman',18);
