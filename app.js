d100d36506bb2daeeaef3c52219cdad33ee2e40a
chagoy <angelochagoy@gmail.com>
2018-05-07 12:16:18 -0700

    infinity

--- app.js
+++ app.js
@@ -10,3 +10,22 @@ function jediName(firstName, lastName) {
 }
 
 jediName('Joseph', 'Kim');
+
+function infinity(num)
+{
+    console.log(num);
+    if (!isFinite(num)) {
+        console.log('And beyond')
+    } else if (num < 0) {
+        console.log('To negative infinity');
+    } else if (num > 0) {
+        console.log('To infinity');
+    } else {
+        console.log('Staying home');
+    }
+}
+
+infinity(0);
+infinity(-5);
+infinity(100);
+infinity(infinity);
